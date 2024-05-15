158e01120c78c3b2fbce89d273dff27e3bd6c9b7
Programmingchuks24 <chuksezeaku@gmail.com>
2024-05-15 01:35:22 +0100

    made changes

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -10,12 +10,14 @@ import eighthpic from "./pics/logo-Procore-white.svg";
 import ninethpic from "./pics/logo-Ramp-white.svg";
 import tenthpic from "./pics/logo-Writer-white.svg";
 import crowdriff from "./pics/logo-CrowdRiff-lightpink.svg";
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import courtney from "./pics/customer-courtney-malinas.png";
 
 function Mainpage() {
   return (
-    <div className="w-auto h-[70rem] bg-zinc-900 md:h-auto">
+    <div className="w-auto h-[76rem] bg-zinc-900 md:h-auto sm:h-[70rem]">
 
-        <div className = "grid grid-cols-2 grid-rows-5 gap-14 pt-9  items-center justify-items-center md:grid-cols-5  md:pt-14 md:w-[80rem] md:pl-12">
+        <div className = "grid grid-cols-2 grid-rows-5 gap-14 pt-9 pl-2 pr-2 items-center justify-items-center md:grid-cols-5  md:pt-14 md:w-[80rem] md:pl-12">
 
             <img src = {thirdpic}  alt = "thirdpic"/>
             <img src = {eighthpic} alt = "eightpic"/>
@@ -36,6 +38,20 @@ function Mainpage() {
 
             <div className = "w-[27rem] md:w-[53rem] sm:w-[40rem] md:-mt-4">
                 <p className = "text-3xl text-pink-600 font-extrabold text-wrap md:text-4xl">“Salesforce is the source of truth but we need to know that the information in it is accurate. Dooly holds our team accountable 100% of the time. As we continue to grow, it has helped the team to form that habit of accountability and keep deal information quality.”</p>
+            </div>
+
+            <div className= "flex gap-2 mt-5 cursor-pointer hover:text-pink-600 text-pink-400 transition duration-300 ease-in-out">
+                <ArrowCircleRightOutlinedIcon sx={{fontSize:27}}/>
+                <p className = "text-[17px] font-bold">READ MORE</p>
+            </div>
+
+            <div className="flex gap-4 mt-7 ">
+
+                <img src = {courtney} alt = "courtney" className = "w-10 h-10"/>
+                <div>
+                    <p className="text-white text-base font-bold">Courtney Malinas</p>
+                    <p className = "text-white">CrowdRiff</p>
+                </div>
             </div>
         </div>
     </div>

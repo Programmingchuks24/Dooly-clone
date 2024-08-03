714e6efcdbfdc64d6d8b4b32e4581a1f36cebbe5
Programmingchuks24 <chuksezeaku@gmail.com>
2024-08-03 13:38:08 +0100

    changed sizing pt6

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <Header />
       
     </div> */}
-    <div className = "mt-[5rem] overflow-hidden"> 
+    <div className = "mt-[0rem] overflow-hidden"> 
       <Mainheader/> 
       {/* <Section/> */}
       <PageSection/>
